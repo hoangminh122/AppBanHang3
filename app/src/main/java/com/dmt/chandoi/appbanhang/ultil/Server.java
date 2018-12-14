d45ba0253e4c69d374065ba4e8139ee9e249c9f9
hoangminh122 <36332084+hoangminh122@users.noreply.github.com>
2018-12-14 23:22:59 +0700

    Initial commit

---
+++ app/src/main/java/com/dmt/chandoi/appbanhang/ultil/Server.java
@@ -0,0 +1,19 @@
+package com.dmt.chandoi.appbanhang.ultil;
+
+/**
+ * Created by Admin on 4/14/2018.
+ */
+
+public class Server {
+    public static String localhost="192.168.1.4";
+    public static String duongdanloaisp = "http://"+localhost+"/SourceOpen/public/getloaisp.php";
+    public static String duongdansanphammoinhat = "http://"+localhost+"/SourceOpen/public/getsanphammoinhat.php";
+    public static String duongdandienthoai = "http://"+localhost+"/SourceOpen/public/getsanpham.php?page=";
+   // public static String duongdanBanhPizza = "http://"+localhost+"/SourceOpen/public/getsanpham.php?page=";
+
+
+
+  //  public static String duongdanlaptop = "http://"+localhost+"/SourceOpen/public/chitietdonhang.php";
+    public static String duongdandonhang = "http://"+localhost+"/SourceOpen/public/thongtinkhachhang.php";
+    public static String duongdanchitietdonhang = "http://"+localhost+"/SourceOpen/public/chitietdonhang.php";
+}
