e772611bed1e53864304ca47b6655f6482edcbdf
hoangminh122 <36332084+hoangminh122@users.noreply.github.com>
2018-12-15 00:13:35 +0700

    hoan tat 99%

--- app/src/main/java/com/dmt/chandoi/appbanhang/ultil/Server.java
+++ app/src/main/java/com/dmt/chandoi/appbanhang/ultil/Server.java
@@ -5,15 +5,17 @@ package com.dmt.chandoi.appbanhang.ultil;
  */
 
 public class Server {
-    public static String localhost="192.168.1.4";
-    public static String duongdanloaisp = "http://"+localhost+"/SourceOpen/public/getloaisp.php";
-    public static String duongdansanphammoinhat = "http://"+localhost+"/SourceOpen/public/getsanphammoinhat.php";
-    public static String duongdandienthoai = "http://"+localhost+"/SourceOpen/public/getsanpham.php?page=";
+    public static String localhost="nevergiveup1.000webhostapp.com";
+   // public static String duongdanloaisp = "https://"+localhost+"/getloaisp.php";
+   public static String duongdanloaisp = "http://nevergiveup1.000webhostapp.com/getloaisp.php";
+   // public static String duongdansanphammoinhat = "https://"+localhost+"/getsanphammoinhat.php";
+   public static String duongdansanphammoinhat = "http://nevergiveup1.000webhostapp.com/getsanphammoinhat.php";
+    public static String duongdandienthoai = "http://"+localhost+"/getsanpham.php?page=";
    // public static String duongdanBanhPizza = "http://"+localhost+"/SourceOpen/public/getsanpham.php?page=";
 
 
 
   //  public static String duongdanlaptop = "http://"+localhost+"/SourceOpen/public/chitietdonhang.php";
-    public static String duongdandonhang = "http://"+localhost+"/SourceOpen/public/thongtinkhachhang.php";
-    public static String duongdanchitietdonhang = "http://"+localhost+"/SourceOpen/public/chitietdonhang.php";
+    public static String duongdandonhang = "http://"+localhost+"/thongtinkhachhang.php";
+    public static String duongdanchitietdonhang = "http://"+localhost+"/chitietdonhang.php";
 }
